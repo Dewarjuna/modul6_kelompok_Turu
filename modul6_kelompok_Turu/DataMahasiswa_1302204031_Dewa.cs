c0f8fa819802a6386f06b4ec96efd1d7bffe0bf7
Dewarjuna <100781488+Dewarjuna@users.noreply.github.com>
2022-04-02 14:39:50 +0700

    menambahkan class DataMahasiswa_1302204031_Dewa

---
+++ modul6_kelompok_Turu/DataMahasiswa_1302204031_Dewa.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_Turu
+{
+    internal class DataMahasiswa_1302204031_Dewa
+    {
+        public class data1
+        {
+            public string firstname;
+            public string lastname;
+            public string gender;
+            public int age;
+        }
+
+        public void ReadJSON()
+        {
+
+            string dataStr = File.ReadAllText(@"C:\Users\Dewa\source\repos\modul6_kelompok_Turu\jurnal6_1_1302204031.json");
+            //data1 dataMahasiswa = JsonSerializer.Deserialize<data1>(dataStr);
+            data1 dataMahasiswa = JsonConvert.DeserializeObject<data1>(dataStr);
+
+
+            Console.WriteLine($"Nama {dataMahasiswa.firstname} {dataMahasiswa.lastname} berusia {dataMahasiswa.age} dan bejenis kelamin {dataMahasiswa.gender}");
+        }
+    }
+}
--- modul6_kelompok_Turu/Program.cs
+++ modul6_kelompok_Turu/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul6_kelompok_Turu
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataMahasiswa_1302204031_Dewa data = new DataMahasiswa_1302204031_Dewa();
+            data.ReadJSON();
+
+            Console.WriteLine("");
+        }
+    }
+}
