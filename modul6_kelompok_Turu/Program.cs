692baa4cfed74e8a865e98b03c507beb8a4dd5d4
ANWAR YUSUF <anwaryusuf@365.telkomuniversity.ac.id>
2022-04-02 13:45:46 +0700

    first commit

--- modul6_kelompok_Turu/Program.cs
+++ modul6_kelompok_Turu/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul6_Kelompok_08
+{
+	internal class DataMahasiswa
+	{
+		public static void main readJSON()
+		{
+			String jsonString = File.ReadAllText("C:\Users\user\Anwar Yusuf\jurnal 6\modul6_kelompok_Turu");
+
+			dynamic data = JsonConvert.DeserializeObject(jsonString);
+
+
+
+			Console.WriteLine("Nama : " + data.firstName + " " + data.lastName);
+			Console.WriteLine("Gender : " + data.gender);
+			Console.WriteLine("Umur : " + data.age);
+			Console.WriteLine("Alamat : " + data.address.streetAddress + ", " + data.address.city + ", " + data.address.state);
+			Console.WriteLine("Mata Kuliah : ");
+			foreach (var item in data.courses)
+			{
+				Console.WriteLine();
+				Console.WriteLine("Kode matkul : " + item.code);
+				Console.WriteLine("Nama matkul : " + item.name);
+			}
+			Console.WriteLine();
+			return main();
+		}
+	}
+}
